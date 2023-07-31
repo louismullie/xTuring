92d7542e96fc1333e0df176b0eb70e8366348026
L <louis.mullie@gmail.com>
2023-07-31 00:13:02 -0400

    Update causal.py

--- src/xturing/engines/causal.py
+++ src/xturing/engines/causal.py
@@ -224,7 +224,7 @@ class CausalLoraKbitEngine(CausalEngine):
                 load_in_4bit=True,
                 bnb_4bit_quant_type='nf4',
                 bnb_4bit_use_double_quant=True,
-                bnb_4bit_compute_dtype=bfloat16
+                bnb_4bit_compute_dtype=torch.bfloat16
             )
             
             model = AutoModelForCausalLM.from_pretrained(
